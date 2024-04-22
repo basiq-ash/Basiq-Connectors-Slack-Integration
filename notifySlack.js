5bc3e59eb5d229d685703712c295e82b2bdcf6b6
basiq-ash <ashman@basiq.io>
2024-04-22 17:13:46 +1000

    Added NotifySlack param

--- notifySlack.js
+++ notifySlack.js
@@ -1,11 +1,11 @@
 const { WebClient } = require('@slack/web-api');
-const { fetchAndNotify } = require('./fetchAndNotify'); // Assuming the function is in fetchAndNotify.js
+const { fetchAndNotify } = require('./fetchAndNotify');
 
 const slackClient = new WebClient(process.env.SLACK_TOKEN);
 
-async function notifySlack() {
+async function notifySlack(message) {
     try {
-        await fetchAndNotify(slackClient);
+        await fetchAndNotify(slackClient, notifySlack);
     } catch (error) {
         console.error('Slack Error:', error);
     }
