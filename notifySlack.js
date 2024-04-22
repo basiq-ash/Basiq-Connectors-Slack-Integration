e1a3f0450d9b802d331c42c75a2f0088341470cc
basiq-ash <ashman@basiq.io>
2024-04-22 17:16:33 +1000

    Added NotifySlack param

--- notifySlack.js
+++ notifySlack.js
@@ -3,9 +3,17 @@ const { fetchAndNotify } = require('./fetchAndNotify');
 
 const slackClient = new WebClient(process.env.SLACK_TOKEN);
 
-async function notifySlack(message) {
+async function notifySlack() {
     try {
-        await fetchAndNotify(slackClient, notifySlack);
+        const messages = await fetchAndNotify(slackClient);
+        if (messages.length > 0) {
+            for (const message of messages) {
+                await slackClient.chat.postMessage({
+                    channel: process.env.SLACK_CHANNEL_ID,
+                    text: message,
+                });
+            }
+        }
     } catch (error) {
         console.error('Slack Error:', error);
     }
